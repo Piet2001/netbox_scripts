ab8182c8b4d0e73ce31f277bee16c2bd7be334aa
Piet2001 <54183168+Piet2001@users.noreply.github.com>
2024-12-21 22:10:42 +0100

    Create disable_site.py

---
+++ disable_site.py
@@ -0,0 +1,30 @@
+from extras.scripts import *
+
+from dcim.models import Site
+from dcim.choices import SiteStatusChoices
+from ipam.models import Prefix
+from ipam.choices import PrefixStatusChoices
+
+
+class SiteStatusBulkUpdater(Script):
+    class Meta:
+        name = "Disable site"
+
+    site_name = ObjectVar(
+        description="Site To Update",
+        model=Site,
+        required=True
+    )
+
+    def run(self, data, commit):
+
+        # Update the site status
+
+        site = Site.objects.get(name=data['site_name'])
+        if (site.status == SiteStatusChoices.STATUS_RETIRED):
+
+          for prefix in Prefix.objects.filter(site=data['site_name']):
+              prefix.status=PrefixStatusChoices.STATUS_DEPRECATED
+              prefix.save()
+              self.log_success(f"Updated Status of Prexix {prefix} to {data['location_status']}")
+  
